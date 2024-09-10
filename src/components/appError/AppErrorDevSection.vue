04392863107a6fdc816c96bc9a19bfaa29654e47
Nachitous <nachitous@gmail.com>
2024-09-10 21:48:58 +0200

    Users

---
+++ src/components/appError/AppErrorDevSection.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{
+  message: string
+  customCode: number
+  statusCode: number
+  details: string
+  code: string
+  hint: string
+}>()
+</script>
+
+<template>
+  <div>
+    <iconify-icon icon="lucide:triangle-alert" class="error__icon" />
+    <h1 class="error__code">{{ customCode || code }}</h1>
+    <p class="error__code" v-if="statusCode">Status Code: {{ statusCode || code }}</p>
+    <p class="error__msg">{{ message }}</p>
+    <p class="error__msg" v-if="hint">Hint: {{ hint }}</p>
+    <p class="error__msg" v-if="details">Details: {{ details }}</p>
+    <div class="error-footer">
+      <p class="error-footer__text">You'll find lots to explore on the home page.</p>
+      <RouterLink to="/">
+        <Button class="max-w-36"> Back to homepage </Button>
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/components/appError/AppErrorPage.vue
+++ src/components/appError/AppErrorPage.vue
@@ -25,26 +25,25 @@ if (error.value && 'code' in error.value) {
   statusCode.value = error.value.statusCode ?? 0
 }
 
+const ErrorTemplate = import.meta.env.DEV
+  ? defineAsyncComponent(() => import('./AppErrorDevSection.vue'))
+  : defineAsyncComponent(() => import('./AppErrorProdSection.vue'))
+
 router.afterEach(() => {
-  useErrorStore().activeError = null
+  errorStore.clearError()
 })
 </script>
 <template>
   <section class="error">
-    <div>
-      <iconify-icon icon="lucide:triangle-alert" class="error__icon" />
-      <h1 class="error__code">{{ customCode || code }}</h1>
-      <p class="error__code" v-if="statusCode">Status Code: {{ statusCode || code }}</p>
-      <p class="error__msg">{{ message }}</p>
-      <p class="error__msg" v-if="hint">Hint: {{ hint }}</p>
-      <p class="error__msg" v-if="details">Details: {{ details }}</p>
-      <div class="error-footer">
-        <p class="error-footer__text">You'll find lots to explore on the home page.</p>
-        <RouterLink to="/">
-          <Button class="max-w-36"> Back to homepage </Button>
-        </RouterLink>
-      </div>
-    </div>
+    <ErrorTemplate
+      :message
+      :customCode
+      :statusCode
+      :details
+      :code
+      :hint
+      :isCustomError="errorStore.isCustomError"
+    />
   </section>
 </template>
 
@@ -53,23 +52,23 @@ router.afterEach(() => {
   @apply mx-auto flex justify-center items-center flex-1 p-10 text-center -mt-20 min-h-[90vh];
 }
 
-.error__icon {
+:deep(.error__icon) {
   @apply text-7xl text-destructive;
 }
 
-.error__code {
+:deep(.error__code) {
   @apply font-extrabold text-7xl text-secondary;
 }
 
-.error__msg {
+:deep(.error__msg) {
   @apply text-3xl font-extrabold text-primary;
 }
 
-.error-footer {
+:deep(.error-footer) {
   @apply flex flex-col items-center justify-center gap-5 mt-6 font-light;
 }
 
-.error-footer__text {
+:deep(.error-footer__text) {
   @apply text-lg text-muted-foreground;
 }
 
